4638e41af158052fbf8e57e1c9cf856a7257d16b
TK <tk@bradycorp.com>
2023-01-02 13:33:39 -0600

    first commit

---
+++ App/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var counter = 0;
+var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+while (max == -1 || counter < max)
+{
+    Console.WriteLine($"Counter: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
